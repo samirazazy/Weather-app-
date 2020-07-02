508d398677cc14f98d520a972f4e5244b4747baa
samirazazy <samiralazazy@gmail.com>
2020-07-02 19:23:56 +0200

    fetching data from api

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("hello");
+  const apiKey = "548c466d89995062249dd64b89b3c3a5";
+  const query = "London";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    ",uk&units=metric&appid=" +
+    apiKey;
+
+  https.get(url, function (r) {
+    r.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<p>the weather is currently " + description + "</p>");
+      res.write(
+        "<h1>the tempreture in London is " + temp + " degree Celcius</h1>"
+      );
+      res.write("<img src=" + iconUrl + ">");
+      res.send();
+    });
+  });
 });
 
 app.listen(3000, function () {
-  console.log("server runnin");
+  console.log("server is running successfuly");
 });
